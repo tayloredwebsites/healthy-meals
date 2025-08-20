37990d094981ab71049f8d3130a16ca2dc980c4e
Dave Taylor <tayloredwebsites@me.com>
2025-08-20 18:20:34 -0400

    github workflow #10

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../../.."))
+sys.path.insert(0, os.path.abspath("../"))
+# sys.path.insert(0, os.path.abspath("./"))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthy_meals.settings")
 django.setup()
 
